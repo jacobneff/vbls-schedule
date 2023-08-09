17d4c66ac22cafea4b0857720b036ad24878ad01
jacobneff <jacobleeneff@gmail.com>
2023-08-09 17:59:05 -0400

    added an input file

--- schedule.cpp
+++ schedule.cpp
@@ -1,10 +1,13 @@
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-int main()
-{
+int main() {
   string request;
+
+  ofstream input;
+  input.open("request.txt");
 
   cout << "What is your schedule request? ";
   cin >> request;
