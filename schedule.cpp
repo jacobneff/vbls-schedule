f6455f9b274451e8e6d5c4d46bb868cfe29fc7b4
jacobneff <jacobleeneff@gmail.com>
2023-08-10 09:39:37 -0400

    added more functionality for asking the schedule

--- schedule.cpp
+++ schedule.cpp
@@ -1,14 +1,21 @@
 #include <fstream>
 #include <iostream>
 
-using namespace std;
-
 int main() {
-  string request;
+  std::string request;
+  const std::string days[7] = {"Monday", "Tuesday",  "Wednesday", "Thursday",
+                               "Friday", "Saturday", "Sunday"};
 
-  ofstream input;
+  std::ifstream input;
   input.open("request.txt");
 
-  cout << "What is your schedule request? ";
-  cin >> request;
+  for (int i = 0; i <= 6; i++) {
+    std::cout << "What is your request for " + days[i] + ": ";
+    std::cin >> request;
+
+    input >> request;
+  }
+
+  while (input) {
+  }
 }
